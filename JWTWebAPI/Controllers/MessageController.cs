4da95b2966b57204bef19cb45d918d2f7ac7d7b1
Fogh1206 <esbenvensel.fogh@gmail.com>
2022-02-25 23:59:21 +0100

    Adding admin role, and only letting users with admin role be able to use Get() method inside MessageController.

---
+++ JWTWebAPI/Controllers/MessageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+
+        [HttpGet, Authorize(Roles = "Admin")]
+        public async Task<ActionResult<string>> Get()
+        {
+            return Ok("Hello");
+        }
+        
+    }
+}
