eec2e934d318ec5db42751865e84367d58fb3d82
Fogh1206 <esbenvensel.fogh@gmail.com>
2022-02-25 21:40:38 +0100

    JWT API side setup

---
+++ JWTWebAPI/Models/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace JWTWebAPI
+{
+    // This is the User data transfer object.
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        
+    }
+}
